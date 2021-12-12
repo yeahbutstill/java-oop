7570713c52af19ff03fae2ace34d0ab6c6573f91
Dani Setiawan <mayadani28@gmail.com>
2021-12-12 17:12:46 +0700

    Exception Multi Try Catch
    Took 10 minutes

--- src/main/java/com/yeahbutstill/demojavaoop/aplikasi/ValidationApp.java
+++ src/main/java/com/yeahbutstill/demojavaoop/aplikasi/ValidationApp.java
@@ -6,10 +6,10 @@ import com.yeahbutstill.demojavaoop.redcordclasss.LoginRequest;
 
 public class ValidationApp {
     public static void main(String[] args) {
-        LoginRequest loginRequest = new LoginRequest(null, null);
+        LoginRequest loginRequest = new LoginRequest(null, "");
         try {
             ValidationUtil.validaate(loginRequest);
-        } catch (ValidationException e) {
+        } catch (ValidationException | NullPointerException e) {
             System.out.println("Terjadi Error Dengan Pesan " + e.getMessage());
         }
     }
--- src/main/java/com/yeahbutstill/demojavaoop/membuatexception/utils/ValidationUtil.java
+++ src/main/java/com/yeahbutstill/demojavaoop/membuatexception/utils/ValidationUtil.java
@@ -4,13 +4,13 @@ import com.yeahbutstill.demojavaoop.membuatexception.ValidationException;
 import com.yeahbutstill.demojavaoop.redcordclasss.LoginRequest;
 
 public class ValidationUtil {
-    public static void validaate(LoginRequest loginRequest) throws ValidationException {
+    public static void validaate(LoginRequest loginRequest) throws ValidationException, NullPointerException {
         if (loginRequest.username() == null) {
-            throw new ValidationException("Username tidak boleh null");
+            throw new NullPointerException("Username tidak boleh null");
         } else if (loginRequest.username().isBlank()) {
             throw new ValidationException("Username tidak boleng kosong");
         } else if (loginRequest.password() == null) {
-            throw new ValidationException("Password tidak boleh null");
+            throw new NullPointerException("Password tidak boleh null");
         } else if (loginRequest.password().isBlank()) {
             throw new ValidationException("Password tidak boleh kosong");
         }
